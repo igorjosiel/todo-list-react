1e840091701b14e2cbb236c98fc5c1a5cac9b2d6
igorjosiel <igorjosielvitalino@gmail.com>
2026-08-26 20:41:53 -0300

    feat: cria o componente ToDoGroup para agrupar itens completos e incompletos e consumindo o contexto dos todos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, use } from "react";
 import { ChecklistsWrapper } from "./components/ChecklistsWrapper";
 import { Container } from "./components/Container";
 import { Dialog } from "./components/Dialog";
@@ -7,58 +7,22 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Heading } from "./components/Heading";
 import { IconPlus, IconSchool } from "./components/icons";
-import { SubHeading } from "./components/SubHeading";
-import { ToDoItem } from "./components/ToDoItem";
-import { ToDoList } from "./components/ToDoList";
 import { TodoForm } from "./components/TodoForm";
-
-// const todos = [
-//   {
-//     id: 1,
-//     description: "JSX e componentes",
-//     completed: false,
-//     createdAt: "2022-10-31"
-//   },
-//   {
-//     id: 2,
-//     description: "Props, state e hooks",
-//     completed: false,
-//     createdAt: "2022-10-31"
-//   },
-//   {
-//     id: 3,
-//     description: "Ciclo de vida dos componentes",
-//     completed: false,
-//     createdAt: "2022-10-31"
-//   },
-//   {
-//     id: 4,
-//     description: "Testes unitários com Jest",
-//     completed: false,
-//     createdAt: "2022-10-31"
-//   }
-// ];
-
-// const completed = [
-//   {
-//     id: 5,
-//     description: "Controle de inputs e formulários controlados",
-//     completed: true,
-//     createdAt: "2022-10-31"
-//   },
-//   {
-//     id: 6,
-//     description: "Rotas dinâmicas",
-//     completed: true,
-//     createdAt: "2022-10-31"
-//   }
-// ];
+import TodoContext from "./components/TodoProvider/TodoContext";
+import { TodoGroup } from "./components/TodoGroup";
 
 function App() {
   const [showDialog, setShowDialog] = useState(false);
+  const { todos, addTodo } = use(TodoContext);
 
   const toggleDialog = () => {
     setShowDialog(!showDialog);
+  }
+
+  const handleFormSubmit = (formData) => {
+    addTodo(formData);
+
+    toggleDialog();
   }
 
   return (
@@ -71,37 +35,19 @@ function App() {
         </Header>
 
         <ChecklistsWrapper>
-          <SubHeading>Para estudar</SubHeading>
-          <ToDoList>
-            {todos.filter(todo => !todo.completed).map(function (t) {
-              return (
-                <ToDoItem
-                  key={t.id}
-                  item={t}
-                  onToggleCompleted={toggleTodoCompleted}
-                  onDeleteTodo={deleteTodo}
-                />
-              );
-            })}
-          </ToDoList>
+          <TodoGroup
+            heading="Para estudar"
+            items={todos.filter(todo => !todo.completed)}
+          />
 
-          <SubHeading>Concluído</SubHeading>
-          <ToDoList>
-            {todos.filter(todo => todo.completed).map(function (t) {
-              return (
-                <ToDoItem
-                  key={t.id}
-                  item={t}
-                  onToggleCompleted={toggleTodoCompleted}
-                  onDeleteTodo={deleteTodo}
-                />
-              );
-            })}
-          </ToDoList>
+          <TodoGroup
+            heading="Concluído"
+            items={todos.filter(todo => todo.completed)}
+          />
 
           <Footer>
             <Dialog isOpen={showDialog} onClose={toggleDialog}>
-              <TodoForm onSubmit={addTodo} />
+              <TodoForm onSubmit={handleFormSubmit} />
             </Dialog>
 
             <FabButton onClick={toggleDialog}>
